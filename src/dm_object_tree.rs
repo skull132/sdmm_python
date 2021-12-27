0f1af28f13a06b8869bec24089711c4f8722ea4d
Erki <erki@skullnet.me>
2021-12-28 00:21:46 +0200

    Let's get some subtypesof going.

--- src/dm_object_tree.rs
+++ src/dm_object_tree.rs
@@ -3,6 +3,7 @@ use dm::Context;
 use dm::objtree::ObjectTree;
 use pyo3::prelude::*;
 use pyo3::{Py, PyResult, Python};
+use pyo3::types::PyList;
 use crate::dm_objects::DmObject;
 
 #[pyclass]
@@ -35,9 +36,49 @@ impl DmObjectTree {
     fn get_path(&self, py: Python, path: String) -> PyResult<Option<Py<DmObject>>> {
         if let Some(object) = self.objtree.find(path.as_str()) {
             let object = DmObject::new(&py, self.objtree.clone(), object);
-            Ok(Some(Py::new(py, object)?))
+            return Ok(Some(Py::new(py, object)?))
         } else {
-            Ok(None)
+            return Err(
+                pyo3::exceptions::PyException::new_err(
+                    format!("Path not found: {}", path)))
+        }
+    }
+
+    fn get_immediate_subtypes(&self, py: Python, path: String) -> PyResult<Py<PyList>> {
+        if let Some(type_ref) = self.objtree.find(path.as_str()) {
+            let mut list = PyList::empty(py);
+
+            for child in type_ref.children() {
+                if child.parent_type().unwrap() == type_ref {
+                    let dm_object = DmObject::new(&py, self.objtree.clone(), child);
+                    let dm_object = Py::new(py, dm_object)?;
+                    list.append(dm_object)?;
+                }
+            }
+
+            return Ok(list.into_py(py));
+        } else {
+            return Err(
+                pyo3::exceptions::PyException::new_err(
+                    format!("Path not found: {}", path)));
+        }
+    }
+
+    fn get_all_subtypes(&self, py: Python, path: String) -> PyResult<Py<PyList>> {
+        if let Some(type_ref) = self.objtree.find(path.as_str()) {
+            let mut list = PyList::empty(py);
+
+            for child in type_ref.children() {
+                let dm_object = DmObject::new(&py, self.objtree.clone(), child);
+                let dm_object = Py::new(py, dm_object)?;
+                list.append(dm_object)?;
+            }
+
+            return Ok(list.into_py(py));
+        } else {
+            return Err(
+                pyo3::exceptions::PyException::new_err(
+                    format!("Path not found: {}", path)));
         }
     }
 }
